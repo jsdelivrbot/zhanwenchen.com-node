7eef1ead6849924326b615b5a343104dfd6796b6
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2017-01-10 14:15:07 -0500

    added pictures for projects and music

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+/////////////////////////////////////
+// MAIN PAGES
+/////////////////////////////////////
+
 router.get('/', function(req, res){
   res.render('index', {
     title: 'Home'
   });
 });
+
 
 router.get('/about', function(req, res){
   res.render('about', {
@@ -31,6 +36,26 @@ router.get('/social', function(req, res){
   });
 });
 
+/////////////////////////////////////
+// MUSIC SUB-PAGES
+/////////////////////////////////////
+
+router.get('/concerto', function(req, res){
+  res.render('concerto', {
+    title: 'Vassar College Concerto Competition Winners\' Concert, May. 6, 2016'
+  });
+});
+
+router.get('/schumann-trio', function(req, res){
+  res.render('schumann-trio', {
+    title: 'Schumann Piano Trio No.3: I. Bewegt, aber nicht zu rasch '
+  });
+});
+
+/////////////////////////////////////
+// PROJECTS SUB-PAGES
+/////////////////////////////////////
+
 
 router.get('/breakout', function(req, res){
   res.render('breakout', {
@@ -38,6 +63,7 @@ router.get('/breakout', function(req, res){
   });
 });
 
+// Duplicate due to older resume
 router.get('/breakout.html', function(req, res){
   res.render('breakout', {
     title: 'Breakout'
@@ -50,10 +76,5 @@ router.get('/zhanwenchen.com', function(req, res){
   });
 });
 
-router.get('/concerto', function(req, res){
-  res.render('concerto', {
-    title: 'Vassar College Concerto Competition Winners\' Concert, May. 6, 2016'
-  });
-});
 
 module.exports = router;
